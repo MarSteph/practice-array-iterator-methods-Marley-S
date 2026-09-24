df409197a6bfe98e5f68e7913d877f4aa5697436
Marley Stephens <marley.stephens@gmail.com>
2026-09-24 15:29:15 -0500

    Completed Task 2

--- array_methods.js
+++ array_methods.js
@@ -4,3 +4,11 @@ fruits.push("orange");
 let firstFruit = fruits.shift();
 fruits.unshift("grape");
 console.log(fruits);
+
+// Task 2
+let colors = ["red", "blue", "green", "blue", "yellow"];
+let includesResult = colors.includes("blue");
+let firstIndex = colors.indexOf("blue");
+let lastIndex = colors.lastIndexOf("blue");
+let result = [includesResult, firstIndex, lastIndex];
+console.log(result);
